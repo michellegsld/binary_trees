9e163a276ade9eb7e1411648ed0da7d3d0a3ef0a
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 15:51:59 -0400

    [Initial] Recursively goes through a tree to print each number in in-order

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Goes through a tree by in-order traversal
+ * @tree: The binary tree to traverse
+ * @func: The function to call for each node
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	if (tree->left)
+		binary_tree_preorder(tree->left);
+
+	func(tree->n);
+
+	if (tree->right)
+		binary_tree_preorder(tree->right);
+}
