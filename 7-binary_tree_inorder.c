fdcc0b3d5ed0479c5f4927b942523ae4034b2510
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 16:15:32 -0400

    [Fix] Add missing parameter

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,10 +11,10 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	if (tree->left)
-		binary_tree_preorder(tree->left);
+		binary_tree_preorder(tree->left, *func);
 
 	func(tree->n);
 
 	if (tree->right)
-		binary_tree_preorder(tree->right);
+		binary_tree_preorder(tree->right, *func);
 }
