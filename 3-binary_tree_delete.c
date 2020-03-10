1325a45a79a85897aab8594ee6bfc383b93ab0bb
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 15:48:50 -0400

    [Initial] Recursively deletes a tree by going left then right

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: The binary tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left)
+		binary_tree_delete(tree->left);
+
+	if (tree->right)
+		binary_tree_delete(tree->right);
+
+	free(tree);
+}
