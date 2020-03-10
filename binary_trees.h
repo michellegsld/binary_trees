4f9cf51447f98dddde6c82f4f1218d5819a7b679
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 15:30:11 -0400

    [Update] Add all mandatory prototypes and some of advanced

--- binary_trees.h
+++ binary_trees.h
@@ -43,4 +43,103 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /* TASK 2 PROTOTYPE */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* TASK 3 PROTOTYPE */
+void binary_tree_delete(binary_tree_t *tree);
+
+/* TASK 4 PROTOTYPE */
+int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* TASK 5 PROTOTYPE */
+int binary_tree_is_root(const binary_tree_t *node);
+
+/* TASK 6 PROTOTYPE */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* TASK 7 PROTOTYPE */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+
+/* TASK 8 PROTOTYPE */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* TASK 9 PROTOTYPE */
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/* TASK 10 PROTOTYPE */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
+/* TASK 11 PROTOTYPE */
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/* TASK 12 PROTOTYPE */
+size_t binary_tree_leaves(const binary_tree_t *tree);
+
+/* TASK 13 PROTOTYPE */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
+/* TASK 14 PROTOTYPE */
+int binary_tree_balance(const binary_tree_t *tree);
+
+/* TASK 15 PROTOTYPE */
+int binary_tree_is_full(const binary_tree_t *tree);
+
+/* TASK 16 PROTOTYPE */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
+/* TASK 17 PROTOTYPE */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+
+/* TASK 18 PROTOTYPE */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+
+
+/* TASK 19 PROTOTYPE */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
+/* TASK 20 PROTOTYPE */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
+/* TASK 21 PROTOTYPE */
+int binary_tree_is_complete(const binary_tree_t *tree);
+
+/* TASK 22 PROTOTYPE */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+
+/* TASK 23 PROTOTYPE */
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+
+/* TASK 24 PROTOTYPE */
+int binary_tree_is_bst(const binary_tree_t *tree);
+
+/* TASK 25 PROTOTYPE */
+bst_t *bst_insert(bst_t **tree, int value);
+
+/* TASK 26 PROTOTYPE */
+bst_t *array_to_bst(int *array, size_t size);
+
+/* TASK 27 PROTOTYPE */
+bst_t *bst_search(const bst_t *tree, int value);
+
+/* TASK 28 PROTOTYPE */
+bst_t *bst_remove(bst_t *root, int value);
+
+/* TASK 30 PROTOTYPE */
+/* TASK 31 PROTOTYPE */
+/* TASK 32 PROTOTYPE */
+/* TASK 33 PROTOTYPE */
+/* TASK 34 PROTOTYPE */
+/* TASK 35 PROTOTYPE */
+/* TASK 36 PROTOTYPE */
+/* TASK 37 PROTOTYPE */
+/* TASK 38 PROTOTYPE */
+/* TASK 39 PROTOTYPE */
+/* TASK 40 PROTOTYPE */
+/* TASK 41 PROTOTYPE */
+/* TASK 42 PROTOTYPE */
+/* TASK 43 PROTOTYPE */
+/* TASK 44 PROTOTYPE */
+/* TASK 45 PROTOTYPE */
+/* TASK 46 PROTOTYPE */
+
 #endif
