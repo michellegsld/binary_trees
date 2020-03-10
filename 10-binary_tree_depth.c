090694dc567585973057bbbd6f800ade81be1b6f
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-10 21:17:38 +0000

    [UPDATE] Find depth of a binary tree

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measures the height of a binary tree
+ * @tree: tree to be measured
+ * Return: 0 if Tree is NULL, else returns height
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	const binary_tree_t *temp = tree;
+	size_t count = 0;
+
+	while (temp->parent)
+	{
+		temp = temp->parent;
+		count++;
+	}
+	return (count);
+}
