0c54479071487e4c91a717a7a2919d5fc7eef13a
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 16:16:44 -0400

    [Fix] Incorrect function name

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -11,10 +11,10 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	if (tree->left)
-		binary_tree_preorder(tree->left, *func);
+		binary_tree_inorder(tree->left, *func);
 
 	func(tree->n);
 
 	if (tree->right)
-		binary_tree_preorder(tree->right, *func);
+		binary_tree_inorder(tree->right, *func);
 }
