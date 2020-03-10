70bafd9eb98fc3f2c0cdfdbb442951191450dec0
Michelle Giraldo <michellegsld@gmail.com>
2020-03-10 16:15:37 -0400

    [Fix] Add missing parameter

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -11,10 +11,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	if (tree->left)
-		binary_tree_postorder(tree->left);
+		binary_tree_postorder(tree->left, *func);
 
 	if (tree->right)
-		binary_tree_postorder(tree->right);
+		binary_tree_postorder(tree->right, *func);
 
 	func(tree->n);
 }
