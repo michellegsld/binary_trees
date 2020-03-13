a5df612aaccf7adb45abc2de1685a22cef732868
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-03-13 19:34:45 +0000

    [UPDATE] Check is tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: tree to be examined
+ * Return: 0 if NULL, or 1 if full
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	const binary_tree_t *temp = tree;
+
+	if (tree == NULL)
+		return (0);
+	
+	if (temp->left == NULL && temp->right == NULL)
+		return (1);
+
+	if (temp->left && temp->right)
+	{
+		return (binary_tree_is_full(temp->left) && binary_tree_is_full(temp->right));
+	}
+	return (0);
+}
